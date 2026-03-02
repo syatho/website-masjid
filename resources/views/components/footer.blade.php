2e7871ced4a4d7f3b52aebaae388ed6fe1f92caa
ahlaiptek <iptekahla@gmail.com>
2026-03-02 11:08:26 +0700

    fix: Remove email section from footer component

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -74,13 +74,6 @@
                             <p>(024) XXX-XXXX</p>
                         </div>
                     </div>
-                    <div class="flex gap-3">
-                        <svg class="w-5 h-5 text-amber-400 flex-shrink-0 mt-0.5" fill="currentColor" viewBox="0 0 20 20"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"/><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"/></svg>
-                        <div>
-                            <p class="font-semibold text-amber-50">Email</p>
-                            <p>info@masjidsyatho.id</p>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
